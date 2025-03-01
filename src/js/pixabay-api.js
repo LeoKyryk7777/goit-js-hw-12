60117c253d49d0ad212ee2e19dfb9942c5ccba85
Leonid Kyryk <leonidkirik7777@gmail.com>
2025-03-01 14:13:56 +0000

    Initial commit

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import { hideLoader } from './render-functions';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { imageTemplate } from './render-functions';
+
+export const refs = {
+  form: document.querySelector('.search-form'),
+  input: document.querySelector('#input'),
+  button: document.querySelector('.search-button'),
+  gallery: document.querySelector('.gallery'),
+  loader: document.querySelector('.loader-box'),
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createImages(query) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '49003886-a24f9c3a0fd607f8ed8b1fc56';
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  };
+  return axios
+    .get(BASE_URL, { params })
+    .then(({ data }) => {
+      if (data.hits.length === 0) {
+        iziToast.info({
+          title: 'No Results',
+          message: `No images found for your search.`,
+          position: 'topRight',
+        });
+        hideLoader();
+      } else {
+        refs.input.value = '';
+        const markup = data.hits.map(imageTemplate).join('');
+
+        refs.gallery.innerHTML = markup;
+        hideLoader();
+        lightbox.refresh();
+      }
+    })
+
+    .catch(error => {
+      hideLoader();
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Error fetching images. Please try again!`,
+        position: 'topRight',
+      });
+    });
+}
